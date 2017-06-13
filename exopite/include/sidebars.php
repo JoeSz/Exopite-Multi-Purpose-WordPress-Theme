a86463856f5c2da27dccfe63025215a7ade84d3c
Joe <ujbeszel@gmail.com>
2017-06-13 17:56:24 +0200

    Move get sidebar id to sidebars.php

--- exopite/include/sidebars.php
+++ exopite/include/sidebars.php
@@ -507,3 +507,40 @@ function spice_get_widget_id($widget_instance) {
 
     }
 }
+
+function exopite_get_sidebar_id() {
+
+    // Get sidebar by ID on single or on archive pages if set by post meta
+    if ( is_singular() || ( isset( $archive_id ) && ! empty( $archive_id ) ) ) {
+
+        /*
+         * Individual page/post settings
+         */
+        $exopite_meta_data = get_post_meta( get_the_ID(), 'exopite_custom_page_options', true );
+        $sidebar_id = ( isset( $exopite_meta_data['exopite-meta-sidebar-id'] ) ) ? $exopite_meta_data['exopite-meta-sidebar-id'] : 'sidebar-1' ;
+
+    } else {
+
+        /*
+         * Settings from admin option framework
+         */
+        $exopite_settings = get_option( 'exopite_options' );
+
+        // Default sidebar
+        $sidebar_id = 'sidebar-1' ;
+
+        // If sidebar is set in options
+        if ( isset( $exopite_settings['exopite-blog-sidebar-id'] ) && is_home() ) {
+            $sidebar_id = $exopite_settings['exopite-blog-sidebar-id'];
+        }
+
+        // If sidebar is set in options
+        if ( isset( $exopite_settings['exopite-blog-sidebar-id'] ) && is_archive() ) {
+            $sidebar_id = $exopite_settings['exopite-blog-sidebar-id'];
+        }
+
+    }
+
+    return $sidebar_id;
+
+}
--- exopite/sidebar.php
+++ exopite/sidebar.php
@@ -9,31 +9,7 @@
 // Exit if accessed directly
 defined('ABSPATH') or die( 'You cannot access this page directly.' );
 
-// Get sidebar by ID on single or on archive pages if set by post meta
-if ( is_singular() || ( isset( $archive_id ) && ! empty( $archive_id ) ) ) {
-
-    /*
-     * Individual page/post settings
-     */
-    $exopite_meta_data = get_post_meta( get_the_ID(), 'exopite_custom_page_options', true );
-    $sidebar_id = ( isset( $exopite_meta_data['exopite-meta-sidebar-id'] ) ) ? $exopite_meta_data['exopite-meta-sidebar-id'] : 'sidebar-1' ;
-
-} else {
-
-    /*
-     * Settings from admin option framework
-     */
-    $exopite_settings = get_option( 'exopite_options' );
-
-    // Default sidebar
-    $sidebar_id = 'sidebar-1' ;
-
-    // If sidebar is set in options
-    if ( isset( $exopite_settings['exopite-blog-sidebar-id'] ) && is_home() ) {
-        $sidebar_id = $exopite_settings['exopite-blog-sidebar-id'];
-    }
-
-}
+$sidebar_id = exopite_get_sidebar_id();
 
 // Check if selected sidebar is active
 if ( ! is_active_sidebar( $sidebar_id ) ) return;
