8b9512fb21c80802340d7a72b66a6bb2b97d9bfc
Joe <ujbeszel@gmail.com>
2017-06-25 20:35:58 +0200

    Update version

--- exopite/functions.php
+++ exopite/functions.php
@@ -10,7 +10,6 @@
 defined('ABSPATH') or die( 'You cannot access this page directly.' );
 /*
  * ToDo:
- * - 300x200 too small if sidebar -> 330*200
  * - custom sidebar for archive (only blog)
  */
 
@@ -87,7 +86,7 @@ ExopiteSettings::setValue( 'allowed-htmls', array(
  * 1.0 - Define constants.
  * ----------------------------------------------------------------------------------------
  */
-define( 'EXOPITE_VERSION',  '20170618' );
+define( 'EXOPITE_VERSION',  '20170625' );
 defined( 'TEMPLATEPATH' ) or define( 'TEMPLATEPATH', get_template_directory() );
 define( 'TEMPLATEURI', get_template_directory_uri() );
 defined( 'STYLESHEETPATH' ) or define( 'STYLESHEETPATH', get_stylesheet_directory() );
