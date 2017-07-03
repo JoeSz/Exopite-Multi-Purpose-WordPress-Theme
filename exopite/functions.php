5d1153c1cdfcaafde7f9c68d1de27b7ebc3efe37
Joe <ujbeszel@gmail.com>
2017-07-03 14:13:33 +0200

    Change version

--- exopite/functions.php
+++ exopite/functions.php
@@ -86,7 +86,7 @@ ExopiteSettings::setValue( 'allowed-htmls', array(
  * 1.0 - Define constants.
  * ----------------------------------------------------------------------------------------
  */
-define( 'EXOPITE_VERSION',  '20170702' );
+define( 'EXOPITE_VERSION',  '20170703' );
 defined( 'TEMPLATEPATH' ) or define( 'TEMPLATEPATH', get_template_directory() );
 define( 'TEMPLATEURI', get_template_directory_uri() );
 defined( 'STYLESHEETPATH' ) or define( 'STYLESHEETPATH', get_stylesheet_directory() );
