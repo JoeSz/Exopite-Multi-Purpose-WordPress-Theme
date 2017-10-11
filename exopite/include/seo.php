3946c1d25f531a734d516899a693ff17fd77db9d
Joe <ujbeszel@gmail.com>
2017-10-11 17:32:00 +0200

    Override WordPress display title function

--- exopite/include/seo.php
+++ exopite/include/seo.php
@@ -355,3 +355,31 @@ if ( ! function_exists( 'exopite_add_noindex' ) ) {
 
     }
 }
+
+/**
+ * Filters wp_title to print a neat <title> tag based on what is being viewed.
+ *
+ * @link https://wordpress.stackexchange.com/questions/228183/custom-attribute-for-the-title-tag-with-wp-title/228225#228225
+ */
+if (has_action('wp_head','_wp_render_title_tag') == 1) {
+    remove_action('wp_head','_wp_render_title_tag',1);
+    add_action('wp_head','custom_wp_render_title_tag_filtered',1);
+}
+
+function custom_wp_render_title_tag_filtered() {
+    if ( function_exists( '_wp_render_title_tag' ) ) {
+        ob_start();
+        _wp_render_title_tag();
+        $titletag = ob_get_contents();
+        ob_end_clean();
+    } else {
+        $titletag = '';
+    }
+    echo apply_filters('wp_render_title_tag_filter',$titletag);
+}
+
+add_filter('wp_render_title_tag_filter','custom_wp_render_title_tag');
+function custom_wp_render_title_tag($titletag) {
+    $titletag = str_replace( '<title>', '<title itemprop="name">', $titletag );
+    return $titletag;
+}
