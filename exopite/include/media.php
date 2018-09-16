89c4cd7606f31c82698fa8baa0afc72a7aba462a
Joe <ujbeszel@gmail.com>
2018-09-16 08:55:21 +0200

    Add media functions for oembed es post formats

--- exopite/include/media.php
+++ exopite/include/media.php
@@ -150,84 +150,6 @@ if ( ! function_exists( 'exopite_create_effect_image_frame' ) ) {
 }
 
 /**
- * Display featured image on post/blog list
- */
-if ( ! function_exists( 'exopite_display_post_thumbnail' ) ) {
-    function exopite_display_post_thumbnail( $class = '', $full_size = false, $caption = '', $multiple_thumbnail = false ) {
-
-        /*
-         * Display thumbnail az image or background-image
-         *
-         * Get settings
-         */
-        $exopite_settings = get_option( 'exopite_options' );
-
-        // Get all thumbnail sizes
-        $thumbnails = get_intermediate_image_sizes();
-
-        /*
-         * Get medium image size
-         *
-         * Check if user definied image size is exist, if not, use dafault
-         */
-        if ( isset( $exopite_settings['exopite-blog-list-thumbnail-size-medium'] ) &&
-             array_key_exists( $exopite_settings['exopite-blog-list-thumbnail-size-medium'] , $thumbnails ) ) {
-            $thumbnail_slug_medium = get_intermediate_image_sizes()[$exopite_settings['exopite-blog-list-thumbnail-size-medium']];
-        } else {
-            $thumbnail_slug_medium = 'blog-list-multiple';
-        }
-
-        // Get full image size
-        if ( isset( $exopite_settings['exopite-blog-list-thumbnail-size-large'] ) &&
-             array_key_exists( $exopite_settings['exopite-blog-list-thumbnail-size-large'] , $thumbnails ) ) {
-            $thumbnail_slug_large = get_intermediate_image_sizes()[$exopite_settings['exopite-blog-list-thumbnail-size-large']];
-        } else {
-            $thumbnail_slug_large = 'blog-list-full';
-        }
-
-        $thumbnail_size = ( $full_size )  ? $thumbnail_slug_large : $thumbnail_slug_medium;
-        $thumbnail_size_class = ( $multiple_thumbnail ) ? ' image-same-size' : '';
-        $thumbnail_container_style = "";
-        $thumbnail_url = "";
-
-        /*
-         * Display featured image (post thumbnail)
-         */
-        if ( ! is_singular() &&
-             ( ! isset( $exopite_settings['exopite-blog-display-thumbnail'] ) ||
-               $exopite_settings['exopite-blog-display-thumbnail'] == true ) &&
-             has_post_thumbnail() ) :
-
-            if ( $multiple_thumbnail && ! post_password_required() ) {
-
-                // If multiple image in the row, then display them in the same size (looks better)
-                $thumbnail_url = wp_get_attachment_image_src( get_post_thumbnail_id(), $thumbnail_size );
-                $url = $thumbnail_url['0'];
-                $thumbnail_container_style = 'background-image: url(' . $url . ');';
-
-            } elseif ( post_password_required() ) {
-
-                $thumbnail_size_class .= ' image-protected';
-
-            }
-            ?>
-            <div class="clearfix entry-thumbnail-container<?php echo $class; ?>">
-                <a href="<?php echo esc_url( get_permalink() ); ?>">
-                    <?php
-
-                    $image = ( ! $multiple_thumbnail && ! post_password_required() ) ? get_the_post_thumbnail( null, $thumbnail_size ) : '';
-
-                    echo exopite_create_effect_image_frame( $image, $caption, ' entry-thumbnail' . $thumbnail_size_class, $thumbnail_container_style );
-
-                    ?>
-                </a>
-            </div><!-- .entry-thumbnail-container -->
-            <?php
-        endif;
-    }
-}
-
-/**
  * Get featured image if exist or post first image
  */
 if ( ! function_exists( 'get_featured_image' ) ) {
@@ -283,3 +205,195 @@ if ( ! function_exists( 'get_post_first_image' ) ) {
     }
 }
 
+
+/**
+ * Display featured image on post/blog list
+ */
+if ( ! function_exists( 'exopite_display_post_thumbnail' ) ) {
+    function exopite_display_post_thumbnail( $class = '', $full_size = false, $caption = '', $multiple_thumbnail = false ) {
+
+        /**
+         * Display thumbnail az image or background-image
+         *
+         * Get settings
+         */
+        $exopite_settings = get_option( 'exopite_options' );
+
+        // Get all thumbnail sizes
+        $thumbnails = get_intermediate_image_sizes();
+
+        /**
+         * Get medium image size
+         *
+         * Check if user definied image size is exist, if not, use dafault
+         */
+        if ( isset( $exopite_settings['exopite-blog-list-thumbnail-size-medium'] ) &&
+             array_key_exists( $exopite_settings['exopite-blog-list-thumbnail-size-medium'] , $thumbnails ) ) {
+            $thumbnail_slug_medium = get_intermediate_image_sizes()[$exopite_settings['exopite-blog-list-thumbnail-size-medium']];
+        } else {
+            $thumbnail_slug_medium = 'blog-list-multiple';
+        }
+
+        // Get full image size
+        if ( isset( $exopite_settings['exopite-blog-list-thumbnail-size-large'] ) &&
+             array_key_exists( $exopite_settings['exopite-blog-list-thumbnail-size-large'] , $thumbnails ) ) {
+            $thumbnail_slug_large = get_intermediate_image_sizes()[$exopite_settings['exopite-blog-list-thumbnail-size-large']];
+        } else {
+            $thumbnail_slug_large = 'blog-list-full';
+        }
+
+        $thumbnail_size = ( $full_size )  ? $thumbnail_slug_large : $thumbnail_slug_medium;
+        $thumbnail_size_class = ( $multiple_thumbnail ) ? ' image-same-size' : '';
+        $thumbnail_container_style = "";
+        $thumbnail_url = "";
+
+        /**
+         * Display featured image (post thumbnail)
+         */
+        if ( ! is_singular() &&
+             ( ! isset( $exopite_settings['exopite-blog-display-thumbnail'] ) ||
+               $exopite_settings['exopite-blog-display-thumbnail'] == true ) ) :
+
+            if ( has_post_thumbnail() && $multiple_thumbnail && ! post_password_required() ) {
+
+                // If multiple image in the row, then display them in the same size (looks better)
+                $thumbnail_url = wp_get_attachment_image_src( get_post_thumbnail_id(), $thumbnail_size );
+                $url = $thumbnail_url['0'];
+                $thumbnail_container_style = 'background-image: url(' . $url . ');';
+
+            } elseif ( has_post_thumbnail() && post_password_required() ) {
+
+                $thumbnail_size_class .= ' image-protected';
+
+            }
+
+            $post_format = get_post_format();
+
+            $media = '';
+
+            /**
+             * If post format is video or audio, display media insted of thumbnail.
+             */
+            $allowed_formats = array( 'video', 'audio' );
+            if ( in_array( $post_format, $allowed_formats ) ) {
+                $media_url = esc_url( get_post_meta( get_the_id(), 'media_thumbnail', true ) );
+
+                switch ( $post_format ) {
+                    case 'video':
+                        $thumbnail_url = ( has_post_thumbnail() ) ? esc_url( wp_get_attachment_image_src( get_post_thumbnail_id( get_the_ID() ), 'full')[0] ) : false;
+
+                        if ( ! empty( $media_url )  && ! post_password_required() ) :
+                            $site_url = get_site_url();
+                            $poster = ( $thumbnail_url ) ? ' poster="' . $thumbnail_url . '"' : '';
+                            $media .= '<div class="clearfix entry-thumbnail-video-container">';
+
+                            if ( substr( $media_url, 0, strlen( $site_url ) ) == $site_url ||
+                                substr( $site_url, 0, 12 ) == '/wp-content/' ) :
+
+                                $media .= '<video class="video" controls' .  $poster . '>';
+                                $media .= '<source src="' . $media_url . '" type="video/mp4" id="vidsrc">';
+                                $media .= esc_html__( 'Your browser does not support HTML5 video.', 'exopite' );
+                                $media .= '</video>';
+
+                            else:
+                                $media .=  wp_oembed_get( $media_url, array( 'width' => 400 ) );
+                            endif;
+
+                            $media .= '</div>';
+
+                        endif;
+                        break;
+                    case 'audio':
+                        $ext = pathinfo( $media_url, PATHINFO_EXTENSION );
+                        $format = '';
+
+                        switch ( $ext ) {
+                            case 'mp3':
+                                $format = 'audio/mpeg';
+                                break;
+                            case 'ogg':
+                                $format = 'audio/ogg';
+                                break;
+                            case 'wav':
+                                $format = 'audio/wav';
+                                break;
+                        }
+
+                        $media .= '<audio class="audio" controls>';
+                        $media .= '<source src="' . $media_url . '" type="' . $format . '">';
+                        $media .= esc_html__( 'Your browser does not support HTML5 audio.', 'exopite' );
+                        $media .= '</audio>';
+                        break;
+                        // If no post format selected
+
+                }
+            }
+
+            if ( empty( $media ) && has_post_thumbnail() ) {
+                $media .= '<a href="' . esc_url( get_permalink() ) . '">';
+                $image = ( ! $multiple_thumbnail && ! post_password_required() ) ? get_the_post_thumbnail( null, $thumbnail_size ) : '';
+                $media .= exopite_create_effect_image_frame( $image, $caption, ' entry-thumbnail' . $thumbnail_size_class, $thumbnail_container_style );
+                $media .= '</a>';
+            }
+            ?>
+                <div class="clearfix entry-thumbnail-container<?php echo $class; ?>">
+                    <?php echo $media; ?>
+                </div><!-- .entry-thumbnail-container -->
+            <?php
+        endif;
+    }
+}
+
+/**
+ * Replace iframe to embed.
+ *
+ * @link https://gist.github.com/amielucha/d23fb0663b3af0565bb9
+ * @link https://www.safaribooksonline.com/library/view/regular-expressions-cookbook/9780596802837/ch08s02.html
+ */
+// add_filter( 'video_embed_html', 'exopite_change_oembed_result', 99, 4 );
+// add_filter( 'embed_oembed_html', 'exopite_change_oembed_result', 99, 4 );
+// add_filter( 'oembed_result', 'exopite_change_oembed_result', 10, 3 );
+if ( ! function_exists( 'exopite_change_oembed_result' ) ) {
+    function exopite_change_oembed_result( $html, $url, $args, $post_id = 0 ) {
+
+        preg_match_all('@(<iframe(?:.*?)</iframe>)@s', $html, $matches, PREG_SET_ORDER);
+
+        $$html = preg_replace(
+            array('/<iframe/', '/<\/iframe>/'),
+            array('<embed', '</embed>'),
+            $html
+        );
+
+        return $html;
+
+    }
+}
+
+/**
+ * HTML Replacement - Support for a custom class attribute in the native gallery shortcode
+ */
+add_filter( 'post_gallery', function( $html, $attr, $instance )
+{
+    add_filter( 'gallery_style', function( $html ) use ( $attr )
+    {
+        if ( isset( $attr['class'] ) || isset( $attr['data-mode'] ) ) {
+
+            $class = ( isset( $attr['class'] ) ) ? esc_attr( $attr['class'] ) . ' ' : '';
+            $mode = ( isset( $attr['data-mode'] ) ) ? 'data-mode="' . esc_attr( $attr['data-mode'] ) . '" ' : '';
+            unset( $attr['class'] );
+
+            // Modify & replace the current class attribute
+            $html = str_replace(
+                "class='gallery ",
+                sprintf(
+                    $mode . "class='gallery %s",
+                    $class
+                ),
+                $html
+            );
+        }
+        return $html;
+    } );
+
+    return $html;
+}, 10 ,3 );
