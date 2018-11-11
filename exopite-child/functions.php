ce1b79fa83d8c978bff7359d812df48ba018a3c1
Joe <ujbeszel@gmail.com>
2018-11-11 10:30:30 +0100

    Enqueue child theme style

--- exopite-child/functions.php
+++ exopite-child/functions.php
@@ -1,1 +1,9 @@
 <?php
+
+function exopite_child_scripts() {
+
+	// enqueue style
+	wp_enqueue_style('exopite-child-style', get_stylesheet_uri());
+
+}
+add_action( 'wp_enqueue_scripts', 'exopite_child_scripts' );
