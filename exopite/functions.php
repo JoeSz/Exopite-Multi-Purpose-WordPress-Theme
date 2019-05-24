a587d0b59e3cde1a0a4aaf7902bdd0dc5bdfd049
Joe <ujbeszel@gmail.com>
2019-05-24 11:23:59 +0200

    Update version

--- exopite/functions.php
+++ exopite/functions.php
@@ -101,7 +101,7 @@ ExopiteSettings::setValue( 'allowed-htmls', array(
  * 1.0 - Define constants.
  * ----------------------------------------------------------------------------------------
  */
-define( 'EXOPITE_VERSION',  '20180919' );
+define( 'EXOPITE_VERSION',  '20190524' );
 defined( 'TEMPLATEPATH' ) or define( 'TEMPLATEPATH', get_template_directory() );
 define( 'TEMPLATEURI', get_template_directory_uri() );
 defined( 'STYLESHEETPATH' ) or define( 'STYLESHEETPATH', get_stylesheet_directory() );
