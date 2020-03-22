38fbfb10f945c49bd816decff9c67e2e5467c9bd
JoeSz <ujbeszel@gmail.com>
2020-03-22 12:53:29 +0100

    Remove from child

--- exopite-child/functions.php
+++ exopite-child/functions.php
@@ -7,17 +7,3 @@ function exopite_child_scripts() {
 	wp_enqueue_style( 'exopite-child-style', get_stylesheet_uri() );
 
 }
-
-add_filter( 'wp_check_filetype_and_ext', 'add_custom_upload_mimes', 10, 4 );
-function add_custom_upload_mimes( $data, $file, $filename, $mimes ) {
-    $allowed_ext = array( 'svg', 'woff', 'woff2', 'ttf' );
-    $ext = array_pop( explode( '.', $filename ) );
-    if ( ! in_array( $ext, $allowed_ext ) ) {
-        return $data;
-    }
-    $wp_filetype = wp_check_filetype( $filename, $mimes );
-    $ext = $wp_filetype['ext'];
-    $type = $wp_filetype['type'];
-    $proper_filename = $data['proper_filename'];
-    return compact( 'ext', 'type', 'proper_filename' );
-}
